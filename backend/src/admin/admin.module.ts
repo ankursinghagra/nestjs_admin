20600e0798b7cc5be15b74b8fcf35235c89ccb23
Ankur Singh <ankursinghagra@gmail.com>
2024-02-11 22:51:09 +0530

    renamed controller folder 2

--- backend/src/admin/admin.module.ts
+++ backend/src/admin/admin.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './dto/user.entity';
 import { UserService } from './service/user.service';
-import { UserController } from './user/user.controller';
+import { UserController } from './controllers/user.controller';
 
 @Module({
   imports: [TypeOrmModule.forFeature([User])],
